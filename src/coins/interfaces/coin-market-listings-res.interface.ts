6511f9323f41f99bb0040f12f37f6a2c32cfbceb
Andrés David Alvarez Torrez <ndralvz@gmail.com>
2024-07-11 12:54:17 -0300

    refactor(interface): :lipstick: changes coin market res interface to readonly

--- src/coins/interfaces/coin-market-listings-res.interface.ts
+++ src/coins/interfaces/coin-market-listings-res.interface.ts
@@ -1,64 +1,64 @@
 export interface CoinMarketListingsRes {
-  status: Status;
-  data: Datum[];
+  readonly status: Status;
+  readonly data: Datum[];
 }
 
 export interface Datum {
-  id: number;
-  name: string;
-  symbol: string;
-  slug: string;
-  num_market_pairs: number;
-  date_added: Date;
-  tags: string[];
-  max_supply: number | null;
-  circulating_supply: number;
-  total_supply: number;
-  infinite_supply: boolean;
-  platform: Platform | null;
-  cmc_rank: number;
-  self_reported_circulating_supply: null;
-  self_reported_market_cap: null;
-  tvl_ratio: null;
-  last_updated: Date;
-  quote: Quote;
+  readonly id: number;
+  readonly name: string;
+  readonly symbol: string;
+  readonly slug: string;
+  readonly num_market_pairs: number;
+  readonly date_added: Date;
+  readonly tags: string[];
+  readonly max_supply: number | null;
+  readonly circulating_supply: number;
+  readonly total_supply: number;
+  readonly infinite_supply: boolean;
+  readonly platform: Platform | null;
+  readonly cmc_rank: number;
+  readonly self_reported_circulating_supply: null;
+  readonly self_reported_market_cap: null;
+  readonly tvl_ratio: null;
+  readonly last_updated: Date;
+  readonly quote: Quote;
 }
 
 export interface Platform {
-  id: number;
-  name: string;
-  symbol: string;
-  slug: string;
-  token_address: string;
+  readonly id: number;
+  readonly name: string;
+  readonly symbol: string;
+  readonly slug: string;
+  readonly token_address: string;
 }
 
 export interface Quote {
-  USD: Usd;
+  readonly USD: Usd;
 }
 
 export interface Usd {
-  price: number;
-  volume_24h: number;
-  volume_change_24h: number;
-  percent_change_1h: number;
-  percent_change_24h: number;
-  percent_change_7d: number;
-  percent_change_30d: number;
-  percent_change_60d: number;
-  percent_change_90d: number;
-  market_cap: number;
-  market_cap_dominance: number;
-  fully_diluted_market_cap: number;
-  tvl: null;
-  last_updated: Date;
+  readonly price: number;
+  readonly volume_24h: number;
+  readonly volume_change_24h: number;
+  readonly percent_change_1h: number;
+  readonly percent_change_24h: number;
+  readonly percent_change_7d: number;
+  readonly percent_change_30d: number;
+  readonly percent_change_60d: number;
+  readonly percent_change_90d: number;
+  readonly market_cap: number;
+  readonly market_cap_dominance: number;
+  readonly fully_diluted_market_cap: number;
+  readonly tvl: null;
+  readonly last_updated: Date;
 }
 
 export interface Status {
-  timestamp: Date;
-  error_code: number;
-  error_message: null;
-  elapsed: number;
-  credit_count: number;
-  notice: null;
-  total_count: number;
+  readonly timestamp: Date;
+  readonly error_code: number;
+  readonly error_message: null;
+  readonly elapsed: number;
+  readonly credit_count: number;
+  readonly notice: null;
+  readonly total_count: number;
 }
